19ecad2ca132233d0ff86342db1abf2521097ceb
Valero004 <72133079+Valero004@users.noreply.github.com>
2021-06-07 11:59:21 +0200

    jgtjdf

---
+++ Codigos/01_Incial/js/01_holamundo.js
@@ -0,0 +1,8 @@
+//comentario simple
+/*
+comentario de varias lineas
+*/
+console.log("Mensaje de comprobación del primer proyecto");
+console.log("Hola mundo");
+console.log("Esto es otra linea");
+console.log();
