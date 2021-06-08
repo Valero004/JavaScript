de62948a953915a1808e8f9a487dc40d0fa9bb31
Valero004 <72133079+Valero004@users.noreply.github.com>
2021-06-08 11:03:02 +0200

    topo

---
+++ Codigos/01_Incial/js/03_operadores.js
@@ -0,0 +1,20 @@
+//operadores de asignación
+let numero = 6;
+//operadores aritméticos
+let suma = 5+8;
+let resta = 5-8;
+let multiplicacion = 5*8;
+let division = 5/8;
+let resto = 5%8;
+
+resta -=4;
+suma+=6;
+multiplicacion*=2
+division/=4;
+resto%=2;
+
+console.log(`La suma total es ${suma} de tipo ${typeof suma}`);
+console.log(`La resta total es ${resta} de tipo ${typeof resta}`);
+console.log(`La multiplicación total es ${multiplicacion} de tipo ${typeof multiplicacion}`);
+console.log(`La división total es ${division} de tipo ${typeof division}`);
+console.log(`La resto total es ${resto} de tipo ${typeof resto}`);
---
+++ Codigos/01_Incial/js/04_interacciones.js
@@ -0,0 +1,40 @@
+/*
+let variable = 4;
+alert(`Esto es un ejemplo de alert, el valor de la variable es ${variable}`);
+let nombre = prompt("Dime como te llamas");
+alert(`El nombre introducido es ${nombre}`);
+
+//simular un login. Pedir por promt user-pass y comprobar si el user e admin y la pass es admin.
+ usuario = prompt("Nombre de ususario");
+ pasword = prompt("Contraseña");
+
+if(usuario == 'admin' && pasword == 'admin'){
+    console.log('el admin ha iniciado');
+}else{
+    alert(`el usuario ${usuario} con contraseña ${pasword} no existe`);
+}
+*/
+/*
+//se piden dos numeros por promt y se muestra en un alert todos los resultados , siempre y cuando ambos numeros sean positivos
+o1 = Number(prompt("Primer operador"));
+o2 = Number(prompt("Segundo operador"));
+
+if ((o1 >= 0 && !isNaN(o1)) && (o2 >= 0 && !isNaN(o2))) {
+  suma = o1 + o2;
+  resta = o1 - o2;
+  multiplicacion = o1 * o2;
+  divission = o1 / o2;
+  resto = o1 % o2;
+  alert(`El resultado de la suma es: ${suma}\n
+    El resultado de la resta es: ${resta}\n
+    El resultado de la multiplicación es: ${multiplicacion}\n
+    El resultado de la división es: ${divission}\n
+    El resultado del resto es: ${resto}\n`);
+} else {
+  alert("Valores no válidos\nRecuerda que los dos operadores deben ser numeros positivos" );
+}
+*/
+n1 = 6;
+n2 = 7;
+let  comparacion = n1 >= n2;
+let comparacionIgualdad = n1 == n2; 
